2b29efe62b60e04c42412344f12fcdd7b1fd07a0
aditya soni <dev.aditya270325@gmail.com>
2026-04-05 14:25:25 +0530

    cors origin changed

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173"
+  origin: "https://https://delta-notes-lovat.vercel.app/"
 }))
 
 app.use("/api/v1/notes", NoteRoute); 
